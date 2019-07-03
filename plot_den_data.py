4c543d456adb1324d7473bc415c2aac9e9732916
MarshallCharles <cjm.pcan2@gmail.com>
2019-07-03 20:20:33 +0200

    Pentagon and Triangle

--- plot_den_data.py
+++ plot_den_data.py
@@ -8,16 +8,6 @@ import time
 import random_numbers as chaos
 import os
 
-# w = 1
-# h = 1
-# d = 70
-# plt.figure(figsize=(w, h))
-# iris_data = np.genfromtxt(
-#     "fractaldata.csv", names=True,
-#     dtype="float", delimiter=",")
-#
-# plt.plot(iris_data["x"], iris_data["y"], "o")
-# plt.savefig("out.png")
 x, y= 0,0
 df = pd.read_csv('fractaldata.csv')
 fig, ax = plt.subplots()
--- random_numbers.py
+++ random_numbers.py
@@ -13,9 +13,37 @@ def random_numbers(options,bounds,iter,cur):
     else:
         cur = halfway(cur, bounds[rand.choice(options)])
         return cur, iter+1
+def numbers_to_file(options,bounds,amt,cur):
+    file = open('fractaldata.csv', 'a')
+    file.write('x,y')
+    file.write('\n')
+    iter = 0
+
+    try:
+        while iter < int(amt):
+            cur,iter = random_numbers(options,bounds,iter,cur)
+            file.write(str(cur[0])+','+str(cur[1]))
+            file.write('\n')
+        file.close()
+        print("Done")
+        return
+    except KeyboardInterrupt:
+        print("ITER IS",iter)
 
 def main():
-    bounds = [(0,0),(1,0),(.5,1)]
-    random_numbers(bounds)
+    '''#TRIANGLE'''
+    # cur = [0,0]
+    # bounds = [(0,0),(1,0),(.5,1)]
+
+    '''#PENTAGON'''
+    bounds = [(.2,0),(.8,0),(.5,1),(0,.6),(1,.6)]
+    cur = [.2,0]
+    
+    options = []
+    for i in range(len(bounds)):
+        options.append(i)
+    file = open('fractaldata.csv', 'w').close()
+    amt = input("How many datapoints: ")
+    numbers_to_file(options,bounds,amt,cur)
 if __name__=='__main__':
     main()
