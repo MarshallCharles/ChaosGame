d686b72a7dbb2555037729ef011173d531063af0
MarshallCharles <cjm.pcan2@gmail.com>
2020-06-06 18:05:01 +0200

    removing useless file

--- random_numbers.py
+++ random_numbers.py
@@ -49,7 +49,7 @@ class dim2:
         self.iter = 0
 
     def halfway(self,a,b):
-        return [(a[0]+b[0])/2, (a[1]+b[1])/2]
+        return [(a[0]+b[0])/2,(a[1]+b[1])/2]
 
     def random_numbers(self):
         if iter == 0:
@@ -62,34 +62,33 @@ class dim2:
             self.iter += 1
 
     def no_prev_vertex(self):
-        iter = 0
         file = open('fractaldata.csv', 'a')
         file.write('x,y')
         file.write('\n')
         try:
-            while iter < int(self.amt):
-                if iter == 0:
+            while self.iter < int(self.amt):
+                if self.iter == 0:
                     file.write(str(self.cur[0])+','+str(self.cur[1]))
                     file.write('\n')
-                    iter += 1
-                elif iter == 1:
+                    self.iter += 1
+                elif self.iter == 1:
                     num = rand.choice(self.options[1:])
                     vertex = self.bounds[rand.choice(self.options[1:])]
-                    cur = halfway(self.cur,self.vertex )
+                    cur = self.halfway(self.cur,vertex )
                     file.write(str(self.cur[0])+','+str(self.cur[1]))
                     file.write('\n')
-                    iter += 1
+                    self.iter += 1
                     prev = self.cur
                 else:
                     num1 = num
-                    options.remove(num)
+                    self.options.remove(num)
                     num = rand.choice(self.options)
-                    options.append(num1)
-                    vertex = bounds[num]
-                    cur = halfway(self.cur, vertex)
+                    self.options.append(num1)
+                    vertex = self.bounds[num]
+                    self.cur = self.halfway(self.cur, vertex)
                     file.write(str(self.cur[0])+','+str(self.cur[1]))
                     file.write('\n')
-                    iter += 1
+                    self.iter += 1
                     prev = self.cur
             file.close()
             print("Done")
@@ -146,32 +145,32 @@ class dim3:
     def no_prev_vertex(self):
         iter = 0
         file = open('fractaldata.csv', 'a')
-        file.write('x,y')
+        file.write('x,y,z')
         file.write('\n')
         try:
-            while iter < int(self.amt):
-                if iter == 0:
+            while self.iter < int(self.amt):
+                if self.iter == 0:
                     file.write(str(self.cur[0])+','+str(self.cur[1]))
                     file.write('\n')
-                    iter += 1
-                elif iter == 1:
+                    self.iter += 1
+                elif self.iter == 1:
                     num = rand.choice(self.options[1:])
                     vertex = self.bounds[rand.choice(self.options[1:])]
-                    cur = halfway(self.cur,self.vertex )
+                    cur = self.halfway(self.cur,vertex )
                     file.write(str(self.cur[0])+','+str(self.cur[1])+','+str(self.cur[2]))
                     file.write('\n')
-                    iter += 1
+                    self.iter += 1
                     prev = self.cur
                 else:
                     num1 = num
-                    options.remove(num)
+                    self.options.remove(num)
                     num = rand.choice(self.options)
-                    options.append(num1)
-                    vertex = bounds[num]
-                    cur = halfway(self.cur, vertex)
+                    self.options.append(num1)
+                    vertex = self.bounds[num]
+                    self.cur = self.halfway(self.cur, vertex)
                     file.write(str(self.cur[0])+','+str(self.cur[1])+','+str(self.cur[2]))
                     file.write('\n')
-                    iter += 1
+                    self.iter += 1
                     prev = self.cur
             file.close()
             print("Done")
@@ -207,44 +206,59 @@ class dim3:
 
 def main():
     flag = 0
-    threeD = 0
+    threeD = False
     parser = argparse.ArgumentParser("Description = Chaos Game Fractals!")
     g = parser.add_mutually_exclusive_group()
     g.add_argument('-n', '--npv', action = 'store_true', help = 'Next vertex cannot be the previous vertex')
     g.add_argument('-b','--neighbor', action = 'store_true',help = 'Cannon move to neighbor vertices')
-    g2 = parser.add_mutually_exclusive_group()
-    g2.add_argument('-td', '--dim3', action = 'store_true', help = '3d fractal plot')
+
     args = parser.parse_args()
     if args.npv: flag = 1
     if args.neighbor: flag = 2
-    if args.dim3: threeD = 1
 
-    '''2D POLYGONS'''
-    '''Triangle'''
-    # bounds = [(0,0),(1,0),(.5,1)]
-    # cur = [0,0]
-    '''Pentagon'''
-    # bounds = [(.2,0),(.8,0),(1,.6),(.5,1),(0,.6)]
-    # cur = [.2,0]
-    '''Square '''
-    # bounds = [(0,0),(0,1),(1,0),(1,1)]
-    # cur = [0,0]
-    '''Diamond'''
-    # bounds = [(.5,0),(1,.5),(0,.5),(.5,1)]
-    # cur = [.5,0]
-    '''Spear'''
-    # bounds = [(.5,0),(1,.2),(0,.2),(.5,1)]
-    # cur = [.5,0]
-    '''Star'''
-    # bounds = [(.33,0),(.66,0),(1,.66),(0,.66),(.5,1)]
-    # cur = [.33,0]
-    ''''''
-    # bounds = [(.4,0),(.6,0),(0,.6),(0,.4),(1,.4),(1,.6),(.4,1),(.6,1)]
-    # cur = [(.4,0)
-    '''3D'''
-    '''TRIANGULAR PRISM'''
-    bounds = [(0,0,0),(1,0,0),(1,0,1),(0,0,1),(.5,1,.5)]
-    cur = [0,0,0]
+    poly = int(input("Which shape to use?\n\
+~~~2D Polygons~~~\n\
+1: Triangle\n\
+2. Square\n\
+3. Diamond\n\
+4. Spear\n\
+5. Pentagon\n\
+6. Star\n\
+7. Unnamed figure\n\
+~~~3D Polygons~~~\n\
+8. Triangular Prism (Pyramid bih)\n\
+9. Cube\n\
+>>>"))
+    if poly == 1:
+        bounds = [(0,0),(1,0),(.5,1)]
+        cur = [0,0]
+    elif poly == 2:
+        bounds = [(0,0),(0,1),(1,0),(1,1)]
+        cur = [0,0]
+    elif poly == 3:
+        bounds = [(.5,0),(1,.5),(0,.5),(.5,1)]
+        cur = [.5,0]
+    elif poly == 4:
+        bounds = [(.5,0),(1,.2),(0,.2),(.5,1)]
+        cur = [.5,0]
+    elif poly == 5:
+        bounds = [(.2,0),(.8,0),(1,.6),(.5,1),(0,.6)]
+        cur = [.2,0]
+    elif poly == 6:
+        bounds = [(.33,0),(.66,0),(1,.66),(0,.66),(.5,1)]
+        cur = [.33,0]
+    elif poly == 7:
+        bounds = [(.4,0),(.6,0),(0,.6),(0,.4),(1,.4),(1,.6),(.4,1),(.6,1)]
+        cur = [.4,0]
+    elif poly == 8:
+        bounds = [(0,0,0),(1,0,0),(1,0,1),(0,0,1),(.5,1,.5)]
+        cur = [0,0,0]
+        threeD = True
+    elif poly == 9:
+        bounds = [(0,0,0),(1,0,0),(1,1,0),(0,1,0),(0,0,1),(0,1,1),(1,0,1),(1,1,1)]
+        cur = [0,0,0]
+        threeD = True
+    else: return
 
     options = []
     for i in range(len(bounds)):
@@ -256,6 +270,6 @@ def main():
         dim3(options,bounds,cur,amt,flag).numbers_to_file()
     else: dim2(options,bounds,cur,amt,flag).numbers_to_file()
 
-    # numbers_to_file(options,bounds,amt,flag)
+
 if __name__=='__main__':
     main()
